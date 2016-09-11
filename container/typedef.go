7c73991eb3ce610b2aa5e35dd30649b587cde7c1
bobliu0909 <bobliu0909@gmail.com>
2016-09-11 14:31:39 +0800

    抽象一个容器value检测方法
    添加Contains，目前只处理切片与map.

--- container/typedef.go
+++ container/typedef.go
@@ -12,6 +12,7 @@ package container
 
 import (
 	"errors"
+	"reflect"
 )
 
 var DEFAULT_STACK_MAXCOUNT = 1024
@@ -23,3 +24,21 @@ var (
 	ERR_QUEUE_FULL  = errors.New("queue is full.")
 	ERR_QUEUE_EMPTY = errors.New("queue is empty.")
 )
+
+func Contains(obj interface{}, target interface{}) bool {
+
+	targetValue := reflect.ValueOf(target)
+	switch reflect.TypeOf(target).Kind() {
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < targetValue.Len(); i++ {
+			if targetValue.Index(i).Interface() == obj {
+				return true
+			}
+		}
+	case reflect.Map:
+		if targetValue.MapIndex(reflect.ValueOf(obj)).IsValid() {
+			return true
+		}
+	}
+	return false
+}
